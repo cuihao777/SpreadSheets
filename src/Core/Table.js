ce451ea7913560c56b1fbe85e9dd6e588be0d0d1
cuihao777 <cuihao871120@gmail.com>
2020-10-05 09:25:31 +0800

    可输出内容部分

--- src/Core/Table.js
+++ src/Core/Table.js
@@ -101,6 +101,7 @@ class Table {
         this.canvas.resize(width, height);
 
         this.renderHeader();
+        this.renderData();
         this.renderGrid();
     }
 
@@ -159,18 +160,74 @@ class Table {
         }
     }
 
+    renderData() {
+        const header = this.dataSet.getHeader();
+        const data = this.dataSet.getData();
+        const { defaultRowHeight, defaultColumnWidth } = Config.Table;
+
+        for (let i = 0, offsetY = defaultRowHeight; i < data.length; i++) {
+            const height = data[i].height || defaultRowHeight;
+            const cells = data[i].cells || [];
+
+            for (let j = 0, offsetX = 0; j < cells.length; j++) {
+                const field = header[j];
+                if (field.width === undefined) {
+                    console.log(i, j);
+                }
+                const cellWidth = field.width || defaultColumnWidth;
+                const cellHeight = height;
+                const align = ["left", "center", "right"].includes(field.align) ? field.align : 'center';
+                const text = cells[j];
+
+                const rect = {
+                    x: offsetX,
+                    y: offsetY,
+                    width: cellWidth,
+                    height: cellHeight
+                };
+
+                const params = {
+                    x: rect.x,
+                    y: Math.trunc(rect.y + rect.height / 2),
+                    textAlign: 'start',
+                    textBaseline: 'middle',
+                    fontSize: 13
+                };
+
+                if (align === 'center') {
+                    params.x = Math.trunc(rect.x + rect.width / 2);
+                    params.textAlign = 'center';
+                } else if (align === 'right') {
+                    params.x = rect.x + rect.width;
+                    params.textAlign = 'end';
+                }
+
+                this.canvas.attr({ fillStyle: "#000000" });
+                this.canvas.fillText({
+                    text: text,
+                    ...params
+                }, rect);
+
+                offsetX += rect.width;
+            }
+
+            offsetY += height;
+        }
+    }
+
     renderGrid() {
         const maxWidth = this.canvas.size.width;
         const maxHeight = this.canvas.size.height;
         const header = this.dataSet.getHeader();
         const data = this.dataSet.getData();
+        const { defaultRowHeight, defaultColumnWidth } = Config.Table;
 
         let rowEndpoint = 0;
         let columnEndpoint = 0;
 
         for (let i = 0; i < header.length; i++) {
             const field = header[i];
-            const fieldWidth = field.width ? field.width : Config.Table.defaultColumnWidth;
+            const fieldWidth = field.width ? field.width : defaultColumnWidth;
 
             rowEndpoint += fieldWidth;
 
@@ -182,7 +239,7 @@ class Table {
 
         for (let i = 0; i < data.length; i++) {
             const row = data[i];
-            columnEndpoint += row.height ? row.height : Config.Table.defaultRowHeight;
+            columnEndpoint += row.height ? row.height : defaultRowHeight;
 
             if (columnEndpoint > maxHeight) {
                 columnEndpoint = maxHeight;
@@ -190,9 +247,14 @@ class Table {
             }
         }
 
-        for (let i = 0, s = 0; i < data.length; i++) {
+        this.canvas.save();
+        this.canvas.setLineStyle();
+        this.canvas.line([0, defaultRowHeight], [rowEndpoint, defaultRowHeight])
+        this.canvas.restore();
+
+        for (let i = 0, s = defaultRowHeight; i < data.length; i++) {
             const row = data[i];
-            const rowHeight = row.height ? row.height : Config.Table.defaultRowHeight;
+            const rowHeight = row.height ? row.height : defaultRowHeight;
             s += rowHeight;
 
             this.canvas.save();
@@ -203,7 +265,7 @@ class Table {
 
         for (let i = 0, s = 0; i < header.length; i++) {
             let column = header[i];
-            const columnWidth = column.width ? column.width : Config.Table.defaultColumnWidth;
+            const columnWidth = column.width ? column.width : defaultColumnWidth;
             s += columnWidth;
 
             this.canvas.save();
