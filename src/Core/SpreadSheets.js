e6b6c44f7cd696f6396f14442ff078e0b59fec49
최호 <cuihao871120@gmail.com>
2020-10-13 08:44:01 +0800

    修改示例头部信息

--- src/Core/SpreadSheets.js
+++ src/Core/SpreadSheets.js
@@ -12,16 +12,26 @@ class SpreadSheets {
         this.options = { ...this.defaultOptions, ...options };
 
         this.options.header = [
-            { title: "Hello World", width: 100 },
-            { title: "Name", width: 100, align: 'left' },
-            { title: "Password", width: 100, align: 'right' },
-            { title: "D", width: 133 },
-            { title: "E", width: 100 },
-            { title: "F", width: 100 },
-            { title: "G", width: 100 },
-            { title: "H", width: 100 },
-            { title: "I", width: 100 },
-            { title: "J", width: 200, align: 'right' }
+            { title: "Code", width: 100 },
+            { title: "Depth 1", width: 80 },
+            { title: "Depth 2", width: 80 },
+            { title: "Depth 3", width: 100 },
+            { title: "Depth 4", width: 130 },
+            { title: "Depth 5", width: 60 },
+            { title: "Depth 6", width: 60 },
+            { title: "TestCase 명", width: 200 },
+            { title: "구분0", width: 40 },
+            { title: "구분1", width: 40 },
+            { title: "구분2", width: 40 },
+            { title: "구분3", width: 40 },
+            { title: "M/A", width: 40 },
+            { title: "통합 테스트 TC", width: 40 },
+            { title: "배포체크리스트", width: 40 },
+            { title: "배포 필수 체크 리스트", width: 40 },
+            { title: "STEP", width: 200, align: 'left' },
+            { title: "기대결과", width: 200, align: 'left' },
+            { title: "라벨", width: 100 },
+            { title: "작성자", width: 100 }
         ];
 
         this.data = [];
